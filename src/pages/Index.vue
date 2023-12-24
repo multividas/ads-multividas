f4b7ba6b839e51f10c896d61e9ca06f8ececeb44
soulaimaneyahya <soulaimaneyh07@gmail.com>
2023-12-24 04:10:16 +0100

    fixing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,16 +7,22 @@
     <main class="container px-4 py-2 my-4 py-md-5 my-md-2">
       <div class="card">
         <div class="card-body">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut repudiandae fuga quo
-            tenetur deserunt ad culpa, odio eaque consectetur laudantium vero quia veniam provident
-            at architecto debitis molestiae perferendis aut sapiente incidunt consequuntur quae
-            sequi dolore rem. Nulla sint, facilis ut laudantium praesentium vero eum accusamus quo
-            perspiciatis nam sapiente molestias amet quam nihil doloribus ea, modi quia suscipit
-            similique possimus molestiae quis, reprehenderit optio temporibus? Earum dolorem
-            incidunt possimus impedit maiores, eaque atque error necessitatibus nesciunt adipisci
-            odit aliquam quidem molestias itaque. Maiores laborum quod sed dolore dolorum aspernatur
-            autem sint eligendi voluptatibus, cupiditate deserunt corrupti facere quos asperiores!
+          <p class="lg-lg mb-2">
+            Multividas is a 'microblogging' system that allows you to send and receive short posts called "threads".
+          </p>
+          <p class="lg-lg mb-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+            repudiandae fuga quo tenetur deserunt ad culpa, odio eaque
+            consectetur laudantium vero quia veniam provident at architecto
+            debitis molestiae perferendis aut sapiente incidunt consequuntur
+            quae sequi dolore rem. Nulla sint, facilis ut laudantium praesentium
+            vero eum accusamus quo perspiciatis nam sapiente molestias amet quam
+            nihil doloribus ea, modi quia suscipit similique possimus molestiae
+            quis, reprehenderit optio temporibus? Earum dolorem incidunt
+            possimus impedit maiores, eaque atque error necessitatibus nesciunt
+            adipisci odit aliquam quidem molestias itaque. Maiores laborum quod
+            sed dolore dolorum aspernatur autem sint eligendi voluptatibus,
+            cupiditate deserunt corrupti facere quos asperiores!
           </p>
         </div>
       </div>
