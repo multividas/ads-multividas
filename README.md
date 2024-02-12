f33100390741865bc01dbc0abe1df3a7973d76b7
Soulaimane Yahya <soulaimaneyh07@gmail.com>
2024-02-12 23:30:41 +0100

    fixing

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Multividas Advertising
 ----- 
 Need helps? Reach me out
 
-> Email: contact@multividas.com
+> Email: multividasdotcom@gmail.com
 
 All the best :beer:
--- src/main.js
+++ src/main.js
@@ -104,7 +104,7 @@ export default function (Vue, { head }) {
     },
     {
       name: 'twitter:site',
-      content: '@multividaseng'
+      content: '@multividascom'
     },
     {
       name: 'twitter:title',
